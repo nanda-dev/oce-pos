44273ee32d805de8ce52cadae22088e622602225
Sandeep Thomas <st328w@att.com>
2017-08-18 12:44:49 +0530

    [Angular] Set Content type as JSON

--- pos-angular/src/app/_services/login.service.ts
+++ pos-angular/src/app/_services/login.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Headers, Response } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class LoginService {
+  private headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8')
+  // private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string) {
@@ -15,7 +17,7 @@ export class LoginService {
         passwrd: password
       },
       service: 'Login'
-    })).map((response: Response) => {
+    }), { headers: this.headers }).map((response: Response) => {
       // login successful if there's a jwt token in the response
       let user = response.json();
       if (user && user.token) {
